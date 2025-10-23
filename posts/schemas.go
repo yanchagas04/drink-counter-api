9213b062f6ec13f22fbf6df056a9ad5e3f4966c5
Yan Chagas <yanchagas2004@gmail.com>
2025-10-23 19:29:21 -0300

    fix: Atualizando os schemas

--- posts/schemas.go
+++ posts/schemas.go
@@ -1,16 +1,25 @@
 package posts
 
-type PostRequest struct {
-	Title string `json:"title"`
-	Description string `json:"description"`
+type CommentData struct {
+	ID uint `json:"id"`
+	Content string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
 }
 
 type PostData struct {
 	ID uint `json:"id"`
 	Title string `json:"title"`
 	Description string `json:"description"`
+	Likes int `json:"likes"`
+	Comments []CommentData `json:"comments"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
+}
+
+type PostRequest struct {
+	Title string `json:"title"`
+	Description string `json:"description"`
 }
 
 type PostResponse struct {
