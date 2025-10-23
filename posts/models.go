1d73b21d673dfd8ee62b558c0a6b2fcc2a5f0b1d
Yan Chagas <yanchagas2004@gmail.com>
2025-10-23 19:01:13 -0300

    fix: Criando comentários dentros dos posts e colocando os IDs

--- posts/models.go
+++ posts/models.go
@@ -4,21 +4,17 @@ import (
 	"gorm.io/gorm"
 )
 
+type Comment struct {
+	gorm.Model
+	ID uint
+	Content string
+}
+
 type Post struct {
 	gorm.Model
+	ID uint
 	Title string
 	Description string
-}
-
-type PostResponse struct {
-	ID uint `json:"id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
-
-type PostRequest struct {
-	Title string `json:"title"`
-	Description string `json:"description"`
+	Likes int
+	Comments []Comment
 }
