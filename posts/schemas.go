6aaaba42a71bb23c79e21b326219785787db9231
yanchagas04 <yanchagas2004@gmail.com>
2025-10-24 03:13:20 -0300

    fix: Retirando os comentários dos schemas de posts

--- posts/schemas.go
+++ posts/schemas.go
@@ -13,7 +13,6 @@ type PostData struct {
 	Title       string        `json:"title"`
 	Description string        `json:"description"`
 	Likes       int           `json:"likes"`
-	Comments    []CommentData `json:"comments"`
 	CreatedAt   string        `json:"created_at"`
 	UpdatedAt   string        `json:"updated_at"`
 }
