155d9d3117d2800e71df3ff3e2d600769f4eca17
yanchagas04 <yanchagas2004@gmail.com>
2025-10-24 02:56:09 -0300

    chore: Prettier

--- posts/schemas.go
+++ posts/schemas.go
@@ -1,34 +1,34 @@
 package posts
 
 type CommentData struct {
-	ID uint `json:"id"`
-	Content string `json:"content"`
-	Author uint `json:"author"`
+	ID        uint   `json:"id"`
+	Content   string `json:"content"`
+	Author    uint   `json:"author"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
 
 type PostData struct {
-	ID uint `json:"id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-	Likes int `json:"likes"`
-	Comments []CommentData `json:"comments"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID          uint          `json:"id"`
+	Title       string        `json:"title"`
+	Description string        `json:"description"`
+	Likes       int           `json:"likes"`
+	Comments    []CommentData `json:"comments"`
+	CreatedAt   string        `json:"created_at"`
+	UpdatedAt   string        `json:"updated_at"`
 }
 
 type PostRequest struct {
-	Title string `json:"title"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
 type PostResponse struct {
-	Message string `json:"message"`
-	Data PostData `json:"data"`
+	Message string   `json:"message"`
+	Data    PostData `json:"data"`
 }
 
 type PostListResponse struct {
-	Message string `json:"message"`
-	Data []PostData `json:"data"`
+	Message string     `json:"message"`
+	Data    []PostData `json:"data"`
 }
